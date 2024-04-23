d9f8fdd93a1da4e05c1ecabab98270ff454cf8d4
shahSpider <aq.syed007@gmail.com>
2024-04-23 18:37:53 +0500

    temp: temporary commit as work in progress

--- textile/textile/report/fabric_ledger/fabric_ledger.py
+++ textile/textile/report/fabric_ledger/fabric_ledger.py
@@ -21,23 +21,42 @@ class FabricLedger:
 
 	def get_data(self):
 		conditions = self.get_conditions()
+		data = []
+
+		balance_cond = ""
+		if self.filters.item_code:
+			balance_cond += " and sle.item_code = '%s'"%self.filters.item_code
+		if self.filters.from_date:
+			balance_cond += " and sle.posting_date < '%s'"%self.filters.from_date
+		if self.filters.warehouse:
+			balance_cond += " and sle.warehouse = '%s'"%self.filters.warehouse
+
+		previous_balance_qty = frappe.db.sql(""" 
+			select ROUND(IFNULL(qty_after_transaction), 2) as qty_after_transaction
+				from `tabStock Ledger Entry` sle
+				inner join `tabItem` item on sle.item_code=item.item_code
+				where (((sle.voucher_type = 'Stock Entry') and ((SELECT stock_entry_type FROM `tabStock Entry` where name=sle.voucher_no) in ('Customer Fabric Receipt', 'Rejected Fabric'))) OR (sle.voucher_type='Stock Reconciliation')) {}
+				order by sle.creation DESC limit 1
+			""".format(balance_cond), as_dict=1)
 		
-		data = frappe.db.sql("""
+		report_query = frappe.db.sql("""
 			select
 				sle.item_code, item.item_name, sle.warehouse, sle.posting_date as date, sle.voucher_type, sle.voucher_no,
-				sle.party_type, sle.party, sle.stock_uom as uom, sle.actual_qty, sle.qty_after_transaction, ste.stock_entry_type
+				sle.party_type, sle.party, sle.stock_uom as uom, ROUND(IFNULL(sle.actual_qty, 0), 2) as actual_qty, ROUND(IFNULL(sle.qty_after_transaction, 0), 2) as qty_after_transaction
 				from `tabStock Ledger Entry` sle
-				inner join `tabStock Entry` ste on sle.voucher_no = ste.name
 				inner join `tabItem` item on sle.item_code=item.item_code
-				where sle.voucher_type='Stock Entry' and ste.stock_entry_type in ('Customer Fabric Receipt', 'Rejected Fabric') {}
+				where (((sle.voucher_type = 'Stock Entry') and ((SELECT stock_entry_type FROM `tabStock Entry` where name=sle.voucher_no) in ('Customer Fabric Receipt', 'Rejected Fabric'))) OR (sle.voucher_type='Stock Reconciliation')) {}
 				order by sle.creation
-		""".format(conditions), as_dict=1)
+		""".format(conditions), as_dict=1, debug=1)
 		
+		data.extend(previous_balance_qty)
+		data.extend(report_query)
 		total_in_qty = 0.0
 		total_out_qty = 0.0
-		balance_qty = data[-1].qty_after_transaction 
-		for d in data:
+		balance_qty = data[-1].qty_after_transaction if data else 0
+		for d in data[1:]:
 			entry_changes = {
+				"purpose": frappe.get_value(d.get("voucher_type"), d.get("voucher_no"), "purpose" if d.voucher_type=="Stock Reconciliation" else "stock_entry_type"),
 				"in_qty" : d.actual_qty if d.actual_qty > 0 else 0,
 				"out_qty" : d.actual_qty if d.actual_qty < 0 else 0, 
 			}
@@ -45,32 +64,17 @@ class FabricLedger:
 			total_in_qty += d.actual_qty if d.actual_qty > 0 else 0
 			total_out_qty += d.actual_qty if d.actual_qty < 0 else 0
 		
-		address_doc_name = frappe.get_cached_value("Dynamic Link", {"link_doctype": "Company", "link_name": self.filters.get("company"), "parenttype": "Address"}, "parent")
 		report_date = (frappe.utils.format_date(frappe.utils.getdate(), "d MMM y")).upper()
 		report_from_date = frappe.utils.format_date(self.filters.from_date, "dd MMM y")
 		report_to_date = frappe.utils.format_date(self.filters.to_date, "dd MMM y")
 
-		balance_cond = ""
-		if self.filters.item_code:
-			balance_cond += " and item_code = '%s'"%self.filters.item_code
-		if self.filters.from_date:
-			balance_cond += " and posting_date < '%s'"%self.filters.from_date
-		if self.filters.warehouse:
-			balance_cond += " and warehouse = '%s'"%self.filters.warehouse
-
-		previous_balance_qty = frappe.db.sql(""" 
-			select qty_after_transaction
-				from `tabStock Ledger Entry`
-				where 1=1 {} order by creation DESC limit 1
-			""".format(balance_cond), as_dict=1)
-
+		
 		print_details = {
 			"report_date": report_date,
 			"report_from_date": report_from_date,
 			"report_to_date": report_to_date,
-			"company_address_doc" : frappe.get_doc("Address", address_doc_name),
-			"total_in_qty": total_in_qty,
-			"total_out_qty": total_out_qty,
+			"total_in_qty": round(total_in_qty, 2),
+			"total_out_qty": round(total_out_qty),
 			"balance_qty": balance_qty,
 			"previous_balance_qty": previous_balance_qty[0].qty_after_transaction if previous_balance_qty else 0,
 		}
@@ -100,7 +104,7 @@ class FabricLedger:
 		columns = [
 			{"label": _("Date"), "fieldname": "date", "fieldtype": "Date", "width": 95},
 			{"label": _("Customer"), "fieldname": "party", "fieldtype": "Link", "options":"Customer", "width": 180, "align": "left"},
-			{"label": _("Voucher Type"), "fieldname": "voucher_type", "width": 110},
+			{"label": _("Voucher Type"), "fieldname": "voucher_type", "width": 150},
 			{"label": _("Voucher #"), "fieldname": "voucher_no", "fieldtype": "Dynamic Link", "options": "voucher_type", "width": 150},
 			{"label": _("Item Code"), "fieldname": "item_code", "fieldtype": "Link", "options": "Item", "width": 200},
 			{"label": _("Item Name"), "fieldname": "item_name", "fieldtype": "Data", "width": 250, "hide_if_filtered": 1},
